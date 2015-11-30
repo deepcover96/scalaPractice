e1720f321ac249d731074ffa45ec0affb77b4806
kaiser <Nolove4U2!!>
2015-11-29 21:09:38 -0700

    11.2 completed.

---
+++ src/test/scala/K_SortingAndSearching/Test_11_2.scala
@@ -0,0 +1,20 @@
+package K_SortingAndSearching
+
+import org.scalatest.FlatSpec
+
+import scala.collection.mutable.ArrayBuffer
+
+
+class Test_11_2 extends FlatSpec {
+  "Calling anagram sorting function" should "return sorted array with anagrams next to each other." in {
+    val arr: ArrayBuffer[String] = ArrayBuffer()
+    arr.append("bad", "bad credit", "car", "dab", "rac", "debit card")
+
+    SortingSearchingTools.anagramSort(arr)
+
+    for(w <- arr) {
+      print(w + " ")
+    }
+
+  }
+}
